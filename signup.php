ce97ae39cff0d4cad0e3b270a122ea2530b44043
jilayjack <jilayshah007@gmail.com>
2017-07-02 23:47:17 +0530

    add1

---
+++ signup.php
@@ -0,0 +1,56 @@
+<?php
+session_start();
+?>
+
+
+<?php 
+
+//if($_SERVER["REQUEST_METHOD"]=="POST"){
+$conn=new mysqli("localhost","root","","miniproject");
+
+$_name=$_POST["txtname"];
+$_email=$_POST["txtemail"];
+$_pass=$_POST["txtpass"];
+$_cpass=$_POST["txtcpass"];
+
+$_mno=$_POST["txtno"];
+$_add=$_POST["txtadd"];
+$_gen=$_POST["txtgender"];
+
+   $_SESSION["Username"]=$_email;
+
+$_img1=$_POST["txtimg"];
+$string=md5(rand());
+$_token=substr($string,0,10);
+$_flag="no";
+
+if($_img1==""){
+    if($_gen=="male"){
+        $_img="avatar5.jpg";
+
+    }
+    else
+    {
+        $_img="log_pic.jpg";
+    }
+}
+else
+{
+    $_img=$_POST["txtimg"];
+}
+//&email=".$email."
+//$message="<a href=newpass.php?token=".$_token.">Click</a>";
+
+      //echo '<a href="login.php?email='.$row["email_id"].'">';
+      $sql="insert into user_tbl values('". $_email ."','". $_name  ."','".$_pass."','".$_cpass."','". $_add ."','". $_mno ."','". $_gen ."','". $_img ."','".$_flag."','".$_token."')";
+if($result=$conn->query($sql)===true){
+
+echo "succes";
+}
+else
+{
+echo $sql;
+    echo "fail";
+}
+
+?>
